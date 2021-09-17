6e2aa7c5eb705e8392d19f5f3d5ed4731b47663c
ambushneupane <iamherovirat@gmail.com>
2021-09-17 10:47:22 +0545

    Arrays used

--- renderVideo.py
+++ renderVideo.py
@@ -3,13 +3,10 @@ from listvideos import videoslist,pathOfVideo
 import os
 
 def renderFinalVideo():
-  # for index,video in enumerate(videoslist):
-  #   videoNames["clip{}".format(index)]= VideoFileClip("/home/ambush/test/"+video)
 
-  videoNames= {f"clip{index}":VideoFileClip(pathOfVideo+video) for index,video in enumerate(videoslist)}
-
-  video_file_clips=list(videoNames.values())
-  final_video = concatenate_videoclips(video_file_clips)
+  videoNames=[VideoFileClip(pathOfVideo+video) for video in videoslist]
+ 
+  final_video = concatenate_videoclips(videoNames)
 
 
   fileName= input("Enter location to save the video with name for your video(only .mp4):-")
